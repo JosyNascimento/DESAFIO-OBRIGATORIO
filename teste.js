7db0451b5210521e46429a90b24a700b3dbbf525
Joseilda Nascimento <joseildatn@gmail.com>
2025-03-04 00:44:15 -0300

    Atualizando .env, .env.example e teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,6 @@
 console.log(__dirname);
-const Message = require('./dao/models/message.model'); // Ajuste o caminho aqui
+const Message = require('./dao/models/message.model'); 
 console.log(Message);
+
+const authController = require("../controllers/auth.controller.js");
+console.log("Auth Controller carregado!", authController);
